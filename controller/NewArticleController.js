e32f6cda3fab97121e1899b2839a2e41235d6897
aicha <aicha@example.com>
2024-06-13 15:00:15 +0200

    add links and validation

--- controller/NewArticleController.js
+++ controller/NewArticleController.js
@@ -8,7 +8,15 @@ const newArticle = (req, res) => {
 const newArticlePost = (req, res) => {
 
     console.log(req.body);
+    const { title, description } = req.body;
 
+     if (!title || title.length > 25) {
+        return res.status(400).send("Title is required and should be a maximum of 25 characters.");
+    }
+
+    if (!description || description.length > 100) {
+        return res.status(400).send("Description is required and should be a maximum of 100 characters.");
+    }
 
     const Data = new Article(req.body);
     Data.save().then(() => {
