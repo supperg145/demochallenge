4dd92a45768fa83e965eb23e0f7345b6a3f9a5ef
SupperGabor <gabor1415@gmail.com>
2024-06-13 17:56:27 +0200

    switched file structure

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,2 @@
 const express = require("express");
 const router = express.Router();
-const routerFunctions = require("../controller/routerFunctions.js");
-
-
-//get the full article
-router.get("/article/:id", routerFunctions.getFullArticle)
-
-//editing the article page
-router.get("/edit/article/:id", routerFunctions.getEditArticlePage)
-router.post("/update/article/:id", routerFunctions.updateArticle)
-
-
-//route for deleting
-router.get("/delete/article/:id", routerFunctions.deleteArticle)
-module.exports = router;
